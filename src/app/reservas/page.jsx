f5f60d73d01357204b0e94c301211799809e932c
= <=>
2024-10-27 21:37:02 -0500

    version4.0

--- src/app/reservas/page.jsx
+++ src/app/reservas/page.jsx
@@ -2,6 +2,7 @@ import Footer from '@/structures/footer/footer'
 import Header from '@/structures/header/header'
 import Reserva from '@/structures/main/reservas/reserva'
 import React from 'react'
+const API_URL = process.env.NEXT_PUBLIC_API_USER;
 
 export default function PageReservas() {
     return (
